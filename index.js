f9ded44a640d8411f39a737737cf0cdaf404471c
DaddySnaddy <86240046+DaddySnaddy@users.noreply.github.com>
2023-10-10 15:26:40 +1100

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+<html>
+  <body>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <span class="fa fa-star star" id="star-0">
+    <span class="fa fa-star star" id="star-1">
+    <span class="fa fa-star star" id="star-2">
+    <span class="fa fa-star star" id="star-3">
+    <span class="fa fa-star star" id="star-4">
+  </body>
+</html>
